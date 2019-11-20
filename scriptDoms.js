94292482d38053e93450676f06fc8b1cc58366c8
Michaella Babbitt <tinydynamine@gmail.com>
2019-11-19 19:24:11 -0600

    fourth commit- still trying to validate entries

--- scriptDoms.js
+++ scriptDoms.js
@@ -1,33 +1,67 @@
 // Write your JavaScript code here!
 function whereWeGoin() {
-   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+        fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
       response.json().then(function(json){
       let destination = json[0];
-      const div = document.getElementById("missionTarget")
+      const div = document.getElementById("missionTarget");
       div.innerHTML = `
          <ol>
-         <li>Name: ${destination.name}</li>
+           <li>Name: ${destination.name}</li>
          <li>Diameter: ${destination.diameter}</li>
          <li>Star: ${destination.star}</li>
          <li>Distance from Earth: ${destination.distance}</li>
          <li>Number of Moons: ${destination.moons}</li>
-       </ol>
+         </ol>
        <img src="${destination.image}">
       `;
    });
 }); 
 
 
+    
+}
+
 
 function validateEntries() {
    let pilotNameInput = document.querySelector("input[name=pilotName]");
+    if (pilotNameInput.value === " ") {
+        pilotNameWarning = "Please enter a name for the Pilot.\n";
+        event.preventDefault();
+    }else{
+        pilotNameWarning= " ";
+    }
+
    let coPilotNameInput = document.querySelector("input[name=coPilotName]");
+    if (coPilotNameInput.value === " "){
+        coPilotNameWarning = "Please enter a name for the co-Pilot.\n";
+        event.preventDefault();
+    }else{
+        coPilotNameWarning = " ";
+    }
+
    let fuelLevelInput = document.querySelector("input[fuel=fuelLevel]");
-   let cargoKgInput = document.querySelector("input[cargo=cargoKg");
-   if (pilotNameInput.value === "" || coPilotNameInput.value === "" || fuelLevelInput.value === "" || cargoKg.value === ""){
+    if (fuelLevelInput.value === " " || fuelLevelInput.value === isNaN){
+        fuelLevelWarning = "Please enter a valid fuel amount. Fuel input must be an integer.\n";
+        event.preventDefault();
+    }else{
+        fuelLevelWarning = " ";
+    }
+   
+    let cargoKgInput = document.querySelector("input[cargo=cargoKg");
+    if (cargoKgInput.value === " " || fuelLevelInput.value === isNaN){
+        cargoKgWarning = "Please enter a valid cargo input amount. Cargo input must be an integer.\n";
+        event.preventDefault();
+    }else{
+        cargoKgWarning = " ";
+    }
+
+
+   if (pilotNameInput.value === "" || coPilotNameInput.value === "" || fuelLevelInput.value === "" || cargoKgInput.value === "") {
       alert ("All fields are required!");
+      event.preventDefault();
    } else if (!(isNaN(pilotNameInput.value)) || !(isNaN(coPilotNameInput.value)) || isNaN(fuelLevelInput.value) || isNaN(cargoKgInput.value)){
       alert ("Data type error! Please review input types");
+      event.preventDefault();
 
    }else{
       let pilotStatus = document.getElementById("pilotStatus");
@@ -36,13 +70,10 @@ function validateEntries() {
       coPilotStatus.innerHTML = (`CoPilot ${coPilotNameInput.value}ready`)
       updateLaunchStatus();
 
-      }
+      
    }   
 
 }
-
-
-
  //      if (fuelLevelInput.value<10000 || cargoKgInput.value>10000){
 //         alert("Shuttle not ready for launch!");
 
